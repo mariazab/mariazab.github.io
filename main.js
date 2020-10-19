884ca078f40a584853281ce387250c46baa59461
mariazab <marysia-zabinska@wp.pl>
2020-10-19 13:47:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//Rellax - parallax effect
+var rellax = new Rellax('.rellax', {
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
+//Animate on scroll
+AOS.init();
